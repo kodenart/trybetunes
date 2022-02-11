95a55a382fbc6e75a6cb4ae2f69698621bccb21c
kodenart <mjuanvitorp@gmail.com>
2022-02-11 13:09:35 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  updateSI = ({ target }) => {
+    this.setState({ searchInput: target.value });
+  }
+
   render() {
+    const { searchInput } = this.state;
+    const arbValueBtn = 2;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          value={ searchInput }
+          onChange={ this.updateSI }
+          data-testid="search-artist-input"
+        />
+        <button
+          type="button"
+          disabled={ searchInput.length < arbValueBtn }
+          data-testid="search-artist-button"
+        >
+          Buscar
+
+        </button>
       </div>
     );
   }
