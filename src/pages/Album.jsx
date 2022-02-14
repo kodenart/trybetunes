7ccc9da938f871daa7c3a3d5076a0f17c4cb471a
kodenart <mjuanvitorp@gmail.com>
2022-02-14 08:34:02 -0300

    requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,63 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      collectionName: '',
+      resMusics: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.gettingMusicsInfo();
+  }
+
+  gettingMusicsInfo = async () => {
+    try {
+    // pegando o valor do parâmetro
+      const { match: { params: { id } } } = this.props;
+      const response = await getMusics(id);
+      // primeiro valor do array são informações do álbum
+      const albumInfo = response[0];
+      // a partir do segundo índice, são as músicas
+      const resMusics = response.slice(1);
+      const { artistName, collectionName } = albumInfo;
+      this.setState({
+        artistName,
+        collectionName,
+        resMusics,
+      });
+    } catch (error) {
+      throw error.message;
+    }
+  }
+
   render() {
+    const { artistName, collectionName, resMusics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <div><h1 data-testid="album-name">{collectionName}</h1></div>
+        <div><h2 data-testid="artist-name">{artistName}</h2></div>
+        <div>
+          {resMusics.map((music) => (
+            <MusicCard
+              key={ music.trackId }
+              trackName={ music.trackName }
+              previewUrl={ music.previewUrl }
+            />))}
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
+};
